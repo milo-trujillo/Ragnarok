fadcd49ca287812ba53c2588fbe7e8297ada37be
Milo Trujillo <milo.trujillo@daylightingsociety.org>
2015-12-02 20:31:03 -0500

    Added packet sniffing host-lister

---
+++ listHosts.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require 'packetfu'
+require 'set'
+
+interface = ARGV[0]
+unless( interface )
+	puts "USAGE: #{__FILE__} <interface name>"
+	exit(0)
+end
+
+sources = Set.new
+
+capture = PacketFu::Capture.new(:iface => interface, :start => true, :filter => "ip")
+capture.stream.each do |pkt|
+	packet = PacketFu::Packet.parse(pkt)
+	source = packet.ip_saddr
+	alreadySeen = sources.add?(source)
+	if( alreadySeen != nil )
+		puts "Received packet from new host #{source}"
+	end
+end
